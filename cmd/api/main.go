2191697c4d7976feede9ab0386a367ee37257a01
slipneff@mail.ru <slipneff@mail.ru>
2024-04-21 03:26:16 +0300

    sdfg

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"hui/internal/server"
-	"os"
-	"strconv"
 
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -14,8 +12,8 @@ func main() {
 	server := server.New()
 
 	server.RegisterFiberRoutes()
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	err := server.Listen(fmt.Sprintf(":%d", port))
+	// port, _ := strconv.Atoi(os.Getenv("PORT"))
+	err := server.Listen(fmt.Sprintf(":%d", 8080))
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,18 +1,54 @@
 package server
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
-	s.App.Get("/", s.HelloWorldHandler)
+	s.App.Get("/", s.GetFileHandler)
 
 }
-
-func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
-	resp := fiber.Map{
-		"message": "Hello World",
+func (s *FiberServer) GetFileHandler(c *fiber.Ctx) error {
+	s3Link := c.Query("link")
+	if s3Link == "" {
+		return c.Status(http.StatusBadRequest).SendString("Не указан URL файла")
 	}
 
-	return c.JSON(resp)
+	resp, err := http.Get(s3Link)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString("Ошибка при загрузке файла: " + err.Error())
+	}
+	defer resp.Body.Close()
+
+	chunkSize := int64(1024 * 1024) // 1 МБ
+	reader := resp.Body
+
+	var totalBytes int64
+	for {
+		buffer := make([]byte, chunkSize)
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		totalBytes += int64(n)
+		// progress := float64(totalBytes) / float64(resp.ContentLength) * 100
+
+		c.Write(buffer[:n])
+
+		// progressJSON := fmt.Sprintf(`{"progress": %.2f}`, progress)
+		// c.Write([]byte(progressJSON))
+
+		buffer = make([]byte, chunkSize)
+	}
+	c.Set("Content-Length", fmt.Sprintf("%d", resp.ContentLength))
+	c.Set("Transfer-Encoding", "chunked")
+	return nil
 }
